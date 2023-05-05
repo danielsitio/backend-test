c780dc1da0446f1b5fed005a063c94b47f740110
danielsitio <danest.esc@gmail.com>
2023-05-05 19:36:53 -0300

    test env

--- src/main/java/com/danest/backend/controller/TestController.java
+++ src/main/java/com/danest/backend/controller/TestController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
-    @GetMapping
+    @GetMapping("/")
     String helloWorld() {
         return "It Works!";
     }
